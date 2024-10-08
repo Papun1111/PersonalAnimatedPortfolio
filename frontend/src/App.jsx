53702f9f28306f85586cafc8dfa1337b63a9d7ef
Papun1111 <gohanmohapatra@gmail.com>
2024-10-08 10:56:53 +0530

    added more animations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import {About,Navbar,Contact,Hero,Tech,Works,StarsCanvas} from "./components"
-import Footer from './components/Footer'
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Navbar,
+  Contact,
+  Hero,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <BrowserRouter>
-    <div className='relative z-0 bg-primary'>
-      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-        <Navbar></Navbar>
-        <Hero></Hero>
-      </div>
-      <About></About>
-      <Tech></Tech>
-      <Works></Works>
-      <div className='relative z-0'>
-        <Contact></Contact>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar></Navbar>
+          <Hero></Hero>
+        </div>
+        <About></About>
+        <Tech></Tech>
+        <Works></Works>
+        <div className="relative z-0">
+          <Contact></Contact>
+        </div>
+        <Footer></Footer>
         <StarsCanvas></StarsCanvas>
       </div>
-      <Footer></Footer>
-    </div>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+export default App;
