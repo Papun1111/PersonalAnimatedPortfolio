e3e462e4546c13dd3040889a27f24c93eae3a88e
Papun1111 <gohanmohapatra@gmail.com>
2024-10-09 10:15:44 +0530

    deleted high resolution images

--- frontend/src/constants/index.js
+++ frontend/src/constants/index.js
@@ -1,27 +1,12 @@
 import {
-    mobile,
     backend,
     creator,
     web,
-    javascript,
-    typescript,
-    html,
-    css,
-    reactjs,
     redux,
     tailwind,
     nodejs,
     mongodb,
     git,
-    figma,
-    docker,
-    meta,
-    starbucks,
-    tesla,
-    shopify,
-    carrent,
-    jobit,
-    tripguide,
     threejs,
   } from "../assets";
   
@@ -56,26 +41,6 @@ import {
   ];
   
   const technologies = [
-    {
-      name: "HTML 5",
-      icon: html,
-    },
-    {
-      name: "CSS 3",
-      icon: css,
-    },
-    {
-      name: "JavaScript",
-      icon: javascript,
-    },
-    {
-      name: "TypeScript",
-      icon: typescript,
-    },
-    {
-      name: "React JS",
-      icon: reactjs,
-    },
     {
       name: "Redux Toolkit",
       icon: redux,
